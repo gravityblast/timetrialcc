c75db03b8036071cccd12c5d23159e75f96cfdb9
Andrea Franz <andrea@gravityblast.com>
2016-02-01 04:21:32 +0000

    redirect to dashboard if logged-in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_strava_user!, only: :landing
 
   def landing
+    if current_user
+      redirect_to  home_path
+    end
   end
 
   def show
