90aba2f276088015bdc75c56ab3ff1d547763c4d
Andrea Franz <andrea@gravityblast.com>
2016-02-03 11:06:42 +0000

    temporarily hack RailsStdoutLogging::StdoutLogger

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 require File.expand_path('../../lib/mini_strava_extensions', __FILE__)
 
+class RailsStdoutLogging::StdoutLogger
+  def broadcast_messages *args; end
+end
+
 module Timetrial
   class Application < Rails::Application
     config.active_job.queue_adapter = :queue_classic
