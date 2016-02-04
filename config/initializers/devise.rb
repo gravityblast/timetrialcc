e76c8a68f2da146c264db73b4018efb36b8ebdad
Andrea Franz <andrea@gravityblast.com>
2016-02-04 13:33:46 +0000

    update strava permission to `public`

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,7 +238,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET'], scope: 'view_private'
+  config.omniauth :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET'], scope: 'public'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
